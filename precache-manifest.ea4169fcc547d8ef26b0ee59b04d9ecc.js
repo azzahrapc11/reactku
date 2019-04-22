c561d73d4bd85bd4c7fc10c0ab141fc5615e7c20
azzahrapc11 <azzahrapramata@gmail.com>
2019-04-22 13:55:15 +0700

    Updates

---
+++ precache-manifest.ea4169fcc547d8ef26b0ee59b04d9ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bad39b6ab6f111edd1f96081053c131",
+    "url": "/react-js-baru/static/media/flash.6bad39b6.svg"
+  },
+  {
+    "revision": "69c5851f2b44fe922595",
+    "url": "/react-js-baru/static/js/runtime~main.50119324.js"
+  },
+  {
+    "revision": "aa9e047d21aecdb0fa01",
+    "url": "/react-js-baru/static/js/main.65fb9ecf.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/react-js-baru/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "aa9e047d21aecdb0fa01",
+    "url": "/react-js-baru/static/css/main.0fc88454.chunk.css"
+  },
+  {
+    "revision": "f04b3520c420551b8565fb8c459833de",
+    "url": "/react-js-baru/index.html"
+  }
+];
